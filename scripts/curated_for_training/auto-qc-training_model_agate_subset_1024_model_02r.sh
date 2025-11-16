ad0e09fc37dd05bf327ca9da792b4523e18a2c87
lundq163 <lundq163@umn.edu>
2025-11-15 21:59:13 -0600

    fix naming error

--- scripts/curated_for_training/auto-qc-training_model_agate_subset_1024_model_02r.sh
+++ scripts/curated_for_training/auto-qc-training_model_agate_subset_1024_model_02r.sh
@@ -14,7 +14,7 @@
 #SBATCH --mail-user=lundq163@umn.edu
 #SBATCH -e logs/automated-qc-Regressor-%j.err
 #SBATCH -o logs/automated-qc-Regressor-%j.out
-#SBATCH -A cdni-nih-bdc
+#SBATCH -A aopitz
 
 cd /users/1/lundq163/projects/automated-qc/src/training || exit
 
@@ -27,7 +27,7 @@ export PYTORCH_ALLOC_CONF=expandable_segments:True
 --model-save-location "/scratch.global/lundq163/auto_qc_model_02r/model_02r.pt" \
 --plot-location "/users/1/lundq163/projects/automated-qc/doc/models/model_02r/model_02r.png" \
 --folder "/scratch.global/lundq163/auto_qc_subset_1024r/" \
---csv-input-file "/users/1/lundq163/projects/automated-qc/data/raw/anat_qc_t1w_t2w_subset_1024r.csv" \
+--csv-input-file "/users/1/lundq163/projects/automated-qc/data/raw/auto_qc_t1w_t2w_subset_1024r.csv" \
 --csv-output-file "/users/1/lundq163/projects/automated-qc/doc/models/model_02r/model_02r.csv" \
 --tb-run-dir "/users/1/lundq163/projects/automated-qc/src/training/runs/" \
 --split-strategy "stratified" \
